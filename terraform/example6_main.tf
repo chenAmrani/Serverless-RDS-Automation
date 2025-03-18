9580231d8cf37c002c81c7c5bc8a7fe9eed2d14a
chenAmrani <chenamrani5@gmail.com>
2025-03-18 15:54:25 +0200

    Delete all examples

--- terraform/example6_main.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_db_instance" "example6" {
-  Database_Name = "example6"
-  Database Engine = "mysql"
-  Environment = "Dev"
-  instance_class = "db.t3.micro"
-  allocated_storage = 20
-}
--- terraform/exampleDBB_main.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_db_instance" "exampleDBB" {
-  Database_Name = "exampleDBB"
-  Database Engine = "mysql"
-  Environment = "Dev"
-  instance_class = "db.t3.micro"
-  allocated_storage = 20
-}
