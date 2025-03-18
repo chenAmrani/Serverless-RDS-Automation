7eeb045ad225ff6a312282b6a795b969a9f532de
chenAmrani <chenamrani5@gmail.com>
2025-03-18 11:20:48 +0200

    Updated config file for improved SAM and Terraform deployment and delete examples

--- terraform/example1_main.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_db_instance" "example1" {
-  Database_Name = "example1"
-  Database Engine = "mysql"
-  Environment = "Dev"
-  instance_class = "db.t3.micro"
-  allocated_storage = 20
-}
