5153e1efb1eb6d0d9b54d0154662a5fcf4051d44
chenAmrani <chenamrani5@gmail.com>
2025-03-19 13:13:50 +0200

    Added missing variables to variables.tf

--- infra/database.tf
+++ infra/database.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "rds_instance" {
   instance_class         = var.environment == "Dev" ? "db.t3.micro" : "db.t3.medium"
   allocated_storage      = var.environment == "Dev" ? 20 : 100
   db_name                = var.db_name
-  username               = var.db_username
+  username               = "admin"
   password               = jsondecode(module.secrets.latest.secret_string)["password"]
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,13 +1,24 @@
-variable "db_name" {}
-variable "db_engine" {}
-variable "environment" {}
-variable "secret_name" {}
-variable "secret_id" {}
-
-variable "db_username" {
-  default = "admin"
+variable "db_name" {
+  description = "Database name for the RDS instance"
+  type        = string
 }
 
-variable "db_password" {
-  default = "StrongPassword1234"
+variable "db_engine" {
+  description = "Database engine type (e.g., MySQL, PostgreSQL)"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment type (e.g., Dev, Prod)"
+  type        = string
+}
+
+variable "secret_name" {
+  description = "The name of the Secrets Manager entry for this database"
+  type        = string
+}
+
+variable "secret_id" {
+  description = "Unique identifier for the AWS Secrets Manager entry"
+  type        = string
 }
